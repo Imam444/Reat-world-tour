4df0f5c15a75b482b69d520a5d5aed8adc8e2b5a
Imam444 <mdimamhossain565528@gmail.com>
2024-10-12 21:48:54 +0600

    added conditional css

--- src/components/Counrtries/Countries.jsx
+++ src/components/Counrtries/Countries.jsx
@@ -1,22 +1,41 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
+import './Countries.css';
 
 
 const Countries = () => {
     const [countries,setCountries] =useState([]);
+    const [visitedCountry, setVisitedCountry] = useState([])
 
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
         .then(data => setCountries(data))
     },[])
+
+    const handleVisitCountry = country =>{
+        console.log(country)
+    }
     return (
-        <div>
+        <div  >
             <h3>Countries:{countries.length}</h3>
-            {
-                countries.map(country =><Country key={country.cca3} country={country}></Country>)
-            }
+            <div>
+                    <h3>Mark Visit</h3>
+                    <ul>
+
+                    </ul>
+            </div>
+            <div className="countries-container">
+                {
+                    countries.map(country =><Country
+                         key={country.cca3} 
+                         handleVisitCountry ={handleVisitCountry}
+                        country={country} 
+                    ></Country>)
+                   
+                }
+            </div>
         </div>
     );
 };
